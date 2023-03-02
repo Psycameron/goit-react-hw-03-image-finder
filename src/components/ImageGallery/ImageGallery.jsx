c2c32905bd9984311534921d4fcde768ebdb5536
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-03-02 19:26:50 +0200

    Refactored ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -40,13 +40,15 @@ export default class ImageGallery extends Component {
     }
 
     if (prevState.page !== this.state.page) {
+      this.setState({ status: 'pending' });
+
       fetch(
         `https://pixabay.com/api/?q=${currentQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
       )
         .then(res => res.json())
         .then(query =>
           this.setState({
-            hits: query.hits,
+            hits: [...prevState.hits, ...query.hits],
             status: 'resolved',
           })
         );
@@ -56,36 +58,36 @@ export default class ImageGallery extends Component {
   handlerLoadMore = () => {
     this.setState(prevState => ({
       page: prevState.page + 1,
-      hits: [...prevState.hits, ...this.state.hits],
     }));
   };
 
   render() {
     const { status, hits, page, totalHits } = this.state;
 
-    if (status === 'idle') {
-      return <div>Введите текст</div>;
-    }
+    // if (status === 'idle') {
+    //   return <div>Введите текст</div>;
+    // }
 
-    if (status === 'pending') {
-      return <Loader />;
-    }
+    // // if (status === 'pending') {
+    // //   return <Loader />;
+    // // }
 
-    if (hits.length < 1) {
-      return <div>Ничего не найдено</div>;
-    }
+    // if (hits.length < 1) {
+    //   return <div>Ничего не найдено</div>;
+    // }
 
-    if (status === 'resolved') {
-      return (
-        <div>
-          <ul className={css.ImageGallery}>
-            <ImageGalleryItem hits={hits} />
-          </ul>
-          {totalHits > 12 * page && (
-            <Button handlerLoadMore={this.handlerLoadMore} />
-          )}
-        </div>
-      );
-    }
+    // if (status === 'resolved') {
+    return (
+      <div>
+        <ul className={css.ImageGallery}>
+          <ImageGalleryItem hits={hits} />
+        </ul>
+        {status === 'pending' && <Loader />}
+        {totalHits > 12 * page && status === 'resolved' && (
+          <Button handlerLoadMore={this.handlerLoadMore} />
+        )}
+      </div>
+    );
+    // }
   }
 }
